754375fa3926d175e329cc6f7b6a73bc6c53a130
apflash256 <hypercube256@naver.com>
2016-02-18 19:14:56 +0900

    버전 이름 붙이기 수정.

--- src/main/java/teamKuiper/redoxiation/Redoxiation.java
+++ src/main/java/teamKuiper/redoxiation/Redoxiation.java
@@ -35,7 +35,7 @@ public class Redoxiation {
 	public static CommonProxy proxy;
 
 	public static final String MODID = "redoxiation";
-	public static final String VERSION = "Alpha 0.05";
+	public static final String VERSION = "1.1-1a4";
 	public static final String NAME = "Redoxiation";
 
 	public static boolean dummybool;
