9e55110c6ef7bf05f4415b4e5462c66e0421d661
Kuiprux <jjh4296@naver.com>
2018-12-10 16:57:46 +0900

    Porting Commands

--- src/main/java/teamKuiper/redoxiation/cmd/CommandCommon.java
+++ src/main/java/teamKuiper/redoxiation/cmd/CommandCommon.java
@@ -3,57 +3,66 @@ package teamKuiper.redoxiation.cmd;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentTranslation;
 
 public class CommandCommon implements ICommand {
-	private List aliases;
+	private List<String> aliases;
 	public CommandCommon() {
-		this.aliases = new ArrayList();
+		this.aliases = new ArrayList<String>();
 		this.aliases.add("redox");
 		this.aliases.add("redoxiation");
 	}
 
 	@Override
-	public int compareTo(Object arg0) {
+	public int compareTo(ICommand arg0) {
 		return 0;
 	}
 
+
 	@Override
-	public String getCommandName() {
+	public String getName() {
 		return "redoxiation";
 	}
 
+
 	@Override
-	public String getCommandUsage(ICommandSender p_71518_1_) {
+	public String getUsage(ICommandSender sender) {
 		return "redoxiation <text>";
 	}
 
+
 	@Override
-	public List getCommandAliases() {
+	public List<String> getAliases() {
 		return this.aliases;
 	}
 
+
 	@Override
-	public void processCommand(ICommandSender icommandsender, String[] argString) {
-		if (icommandsender instanceof EntityPlayer) {
-			EntityPlayer player = (EntityPlayer) icommandsender;
-			if (argString.length == 0) {
-				player.addChatMessage(new ChatComponentTranslation( "[Redoxiation] Hello,  " + player.getGameProfile().getName()));
+	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+		if (sender instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) sender;
+			if (args.length == 0) {
+				player.sendMessage(new TextComponentTranslation( "[Redoxiation] Hello,  " + player.getGameProfile().getName()));
 			}
 		}
-
 	}
 
+
 	@Override
-	public boolean canCommandSenderUseCommand(ICommandSender p_71519_1_) {
+	public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
 		return true;
 	}
 
+
 	@Override
-	public List addTabCompletionOptions(ICommandSender p_71516_1_, String[] p_71516_2_) {
+	public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args,
+			BlockPos targetPos) {
 		return null;
 	}
 
